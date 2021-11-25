635b9ddeab1ae694ae2b37a485e47c4214a1a396
Norbi <info@laszlonorbert.hu>
2021-11-25 22:25:28 +0100

    Initial commit

--- src/Form/FileUploaderType.php
+++ src/Form/FileUploaderType.php
@@ -4,7 +4,7 @@ namespace Lnorby\MediaBundle\Form;
 
 use Lnorby\MediaBundle\Form\DataTransformer\MediaTransformer;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -38,6 +38,6 @@ final class FileUploaderType extends AbstractType
 
     public function getParent()
     {
-        return HiddenType::class;
+        return TextType::class;
     }
 }
