e82565f2c929ebb5bcb120fbf596d5a88689aec5
László Norbert <info@laszlonorbert.hu>
2022-04-09 22:59:46 +0200

    Initial commit

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -47,6 +47,7 @@ class MediaController
         $path = $request->attributes->get('path');
 
         if (preg_match('#([0-9a-f]{10})\.(\d+)x(\d+)([rc])(\.[a-z0-9]+)$#', $path, $matches)) {
+            return new Response($path . ' - ' . $matches[1] . $matches[5]);
             try {
                 $media = $this->mediaRepository->getByPath($matches[1] . $matches[5]);
             } catch (CouldNotFindMedia $e) {
